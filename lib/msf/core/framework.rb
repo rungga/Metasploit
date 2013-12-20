0e7ffc2490298f0e4acdd9e0ba9a88b1a17b79f8
Brandon Turner <bt@brandonturner.net>
2013-12-20 10:00:16 -0600

    Update to version 4.8.2-2013121801

--- lib/msf/core/framework.rb
+++ lib/msf/core/framework.rb
@@ -19,7 +19,7 @@ class Framework
   Major    = 4
   Minor    = 8
   Point    = 2
-  Release  = "-2013121101"
+  Release  = "-2013121801"
 
   if(Point)
     Version  = "#{Major}.#{Minor}.#{Point}#{Release}"
