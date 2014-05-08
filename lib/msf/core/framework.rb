3ff0891e3e620509c3200187a4e276bc9357fa82
Chris Doughty <Chris_Doughty@rapid7.com>
2014-05-08 08:44:43 -0500

    Update to version 4.9.2-2014050701

--- lib/msf/core/framework.rb
+++ lib/msf/core/framework.rb
@@ -19,7 +19,7 @@ class Framework
   Major    = 4
   Minor    = 9
   Point    = 2
-  Release  = "-2014043001"
+  Release  = "-2014050701"
 
   if(Point)
     Version  = "#{Major}.#{Minor}.#{Point}#{Release}"
