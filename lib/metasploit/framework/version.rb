085e848d5f4d8318134cc6094e3cd949409da563
Brandon Turner <bt@brandonturner.net>
2014-08-12 17:18:42 -0500

    Update to version 4.10.0-2014081301

---
+++ lib/metasploit/framework/version.rb
@@ -0,0 +1,13 @@
+module Metasploit
+  module Framework
+    module Version
+      MAJOR = 4
+      MINOR = 10
+      PATCH = 0
+      PRERELEASE = '2014081301'
+    end
+
+    VERSION = "#{Version::MAJOR}.#{Version::MINOR}.#{Version::PATCH}-#{Version::PRERELEASE}"
+    GEM_VERSION = VERSION.gsub('-', '.pre.')
+  end
+end
