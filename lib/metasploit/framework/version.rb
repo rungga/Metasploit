8af48e2fb9c4aa5666d2345f6ae28f49ccf0a913
Brandon Turner <bt@brandonturner.net>
2014-09-26 18:04:50 -0500

    Update to version 4.10.0-2014092602

--- lib/metasploit/framework/version.rb
+++ lib/metasploit/framework/version.rb
@@ -4,7 +4,7 @@ module Metasploit
       MAJOR = 4
       MINOR = 10
       PATCH = 0
-      PRERELEASE = '2014092501'
+      PRERELEASE = '2014092602'
     end
 
     VERSION = "#{Version::MAJOR}.#{Version::MINOR}.#{Version::PATCH}-#{Version::PRERELEASE}"
