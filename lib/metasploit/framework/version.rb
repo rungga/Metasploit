75fe685169d6ffbb834167fb30b33e20d110cc45
Metasploit <metasploit@rapid7.com>
2014-10-03 15:28:09 -0500

    Update to version 4.10.0-2014100201

--- lib/metasploit/framework/version.rb
+++ lib/metasploit/framework/version.rb
@@ -4,7 +4,7 @@ module Metasploit
       MAJOR = 4
       MINOR = 10
       PATCH = 0
-      PRERELEASE = '2014100101'
+      PRERELEASE = '2014100201'
     end
 
     VERSION = "#{Version::MAJOR}.#{Version::MINOR}.#{Version::PATCH}-#{Version::PRERELEASE}"
