6e42d9d60a1ec5e536479159b0f9c7d427df43eb
Metasploit <metasploit@rapid7.com>
2014-11-10 14:42:49 -0600

    Imported Upstream version 4.10.1-2014111001

--- lib/metasploit/framework/version.rb
+++ lib/metasploit/framework/version.rb
@@ -3,8 +3,8 @@ module Metasploit
     module Version
       MAJOR = 4
       MINOR = 10
-      PATCH = 0
-      PRERELEASE = '2014102901'
+      PATCH = 1
+      PRERELEASE = '2014111001'
     end
 
     VERSION = "#{Version::MAJOR}.#{Version::MINOR}.#{Version::PATCH}-#{Version::PRERELEASE}"
