06a58fbd4d8fb7cbfffd5a5d3a1f422b7aaf27c1
Metasploit <metasploit@rapid7.com>
2015-03-03 12:59:38 -0800

    Update to version 4.11.1-2015022303

--- modules/auxiliary/scanner/http/http_login.rb
+++ modules/auxiliary/scanner/http/http_login.rb
@@ -22,10 +22,6 @@ class Metasploit3 < Msf::Auxiliary
     super(
       'Name'           => 'HTTP Login Utility',
       'Description'    => 'This module attempts to authenticate to an HTTP service.',
-      'References'  =>
-        [
-
-        ],
       'Author'         => [ 'hdm' ],
       'References'     =>
         [
@@ -74,6 +70,7 @@ class Metasploit3 < Msf::Auxiliary
         /auth/
         /manager/
         /Management.asp
+        /ews/
       }
     end
 
